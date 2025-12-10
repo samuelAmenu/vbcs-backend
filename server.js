d2df6664a417e2ff013aefaba0c3f3e09748ed68
Samuel <amenuil19@gmail.com>
2025-12-10 11:41:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -251,4 +251,4 @@ app.post('/api/v1/owner/suspend-number', async (req, res) => {
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
-});
+}); 
